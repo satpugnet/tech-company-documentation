63473825c4cf8ddf3e337bc06b577fe0aa4208a7
Pugnet <saturnip@f0189807ce53.ant.amazon.com>
2019-05-20 23:16:10 +0100

    First version of the github interface with an example file

--- src/github_interface/github_interface.py
+++ src/github_interface/github_interface.py
@@ -1,16 +1,23 @@
-from github import Github
+from github import Github, UnknownObjectException
+import base64
 
-# First create a Github instance:
+class GithubInterface:
 
-# using username and password
-g = Github("user", "password")
+    def __init__(self, access_token):
+        self.github_account = Github(access_token)
 
-# or using an access token
-g = Github("access_token")
+    def get_repo_file(self, repo_name, file_name):
+        repo = self.github_account.get_repo(repo_name)
 
-# Github Enterprise with custom hostname
-g = Github(base_url="https://{hostname}/api/v3", login_or_token="access_token")
+        try:
+            encoded_contents = repo.get_contents(file_name)
+        except UnknownObjectException:
+            print("File " + file_name + " not found in " + repo_name)
+            return
 
-# Then play with your Github objects:
-for repo in g.get_user().get_repos():
-    print(repo.name)
+        contents = base64.b64decode(encoded_contents.content).decode("utf-8")
+        return contents
+
+    def get_repos(self):
+        return self.github_account.get_user().get_repos()
+
