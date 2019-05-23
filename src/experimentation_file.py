4e2e65bcab3d1dd8a0412dbfd4cdf105635a6b36
Paul Vidal <paul.vidal@yahoo.fr>
2019-05-24 01:42:40 +0200

    improve github fetching

--- src/experimentation_file.py
+++ src/experimentation_file.py
@@ -1,10 +1,11 @@
 from github_interface.github_interface import GithubInterface
 
 g = GithubInterface("39180cc3f47072520e81a31484291ea5acc5af9f")
-for repo in g.get_repos():
-    print(repo.get_full_name())
-    print(repo.get_root_files()[0].get_type())
+# for repo in g.get_repos():
+#     print(repo.get_full_name())
+#     print(repo.get_root_files()[0].get_type())
     # print(g.get_repo_file(repo.full_name, "README.md"))
 
-print()
-print(g.get_repo("saturnin13/tech-company-documentation").get_file("src/github_interface/github_interface.py").get_content())
+print(g.get_repo("saturnin13/tech-company-documentation")
+      .get_file("src/github_interface/github_interface.py")
+      .get_lines(start=3, stop=5))
--- src/github_interface/types/github_file.py
+++ src/github_interface/types/github_file.py
@@ -6,16 +6,23 @@ from github import GithubException
 class GithubFile:
     def __init__(self, file_object):
         self.__file_object = file_object
+        self.__content = self.__decode_content(file_object)
+        self.__lines = self.__content.splitlines()
 
     def get_content(self):
-        return self.__decode_content(self.__file_object)
+        return self.__content
 
     def get_type(self):
         return self.__file_object.type
 
+    def get_lines(self, start=None, stop=None):
+        start = start if start else 0
+        stop = stop if stop else len(self.__lines) - 1
+        return self.__lines[start:stop+1]
+
     def __decode_content(self, encoded_content):
         try:
             if (encoded_content.content):
-                return base64.b64decode(encoded_content.content)
+                return base64.b64decode(encoded_content.content).decode('utf-8')
         except GithubException as e:
             print("Encoded content " + str(encoded_content) + " faced exception: " + str(e))
