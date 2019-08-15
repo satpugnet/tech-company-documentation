28b6c0afa88de61a1b03f262ef6aba93bc82074e
Paul Vidal <paul.vidal@yahoo.fr>
2019-08-15 15:01:17 +0200

    add prod mongo db

--- backend/mongo/mongo_client.py
+++ backend/mongo/mongo_client.py
@@ -1,7 +1,12 @@
+import os
+
 from pymongo import MongoClient
 
+DEFAULT_URL = 'mongodb://localhost:27017/'
+MONGO_URL = os.getenv('MONGO_URL', DEFAULT_URL)
+
 client = MongoClient(
-    'mongodb://localhost:27017/',
+    MONGO_URL,
     connect=True,  # Connect straight to DB, do not wait for 1st request
     serverSelectionTimeoutMS=3000  # Client timeout
 )
---
+++ set_prod_env.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+export MONGO_URL="mongodb+srv://user:QNBuu71yGEa2lNs6@prod-cluster-hrt4j.gcp.mongodb.net/test?retryWrites=true&w=majority"
