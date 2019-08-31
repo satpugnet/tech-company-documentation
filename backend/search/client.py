27a6d05b5756aa70b48728328e30052871903538
Paul Vidal <paul.vidal@yahoo.fr>
2019-08-31 12:58:23 +0200

    add logging to algolia client

--- backend/search/client.py
+++ backend/search/client.py
@@ -2,17 +2,28 @@ import os
 
 from algoliasearch.search_client import SearchClient
 
+from tools import logger
+
 ALGOLIA_APP_ID = os.getenv("ALGOLIA_APP_ID")
 ALGOLIA_ADMIN_KEY = os.getenv("ALGOLIA_ADMIN_KEY")
 ALGOLIA_SEARCH_KEY = os.getenv("ALGOLIA_SEARCH_KEY")
 
 INDEX_NAME = "test_DOCUMENTATION"
 
-CLIENT = SearchClient.create(ALGOLIA_APP_ID, ALGOLIA_ADMIN_KEY)
-INDEX = CLIENT.init_index(INDEX_NAME)
+try:
+    CLIENT = SearchClient.create(ALGOLIA_APP_ID, ALGOLIA_ADMIN_KEY)
+    INDEX = CLIENT.init_index(INDEX_NAME)
+except Exception:
+    logger.get_logger().exception("Failed to initialise algolia client")
 
 
 def insert_doc(doc):
-    INDEX.save_objects([
-        doc
-    ])
+    success = True
+
+    try:
+        INDEX.save_objects([doc])
+    except Exception:
+        logger.get_logger().exception("Failed to index markdown document %s", doc)
+        success = False
+
+    return success
