99bd76e5589a89867b31a0d765e1bbc7f7386245
Paul Vidal <paul.vidal@yahoo.fr>
2019-08-31 16:40:02 +0200

    add sentry for error reporting

--- backend/server.py
+++ backend/server.py
@@ -9,17 +9,25 @@ from web_server.web_server import web_server
 from webhook.webhook_server import webhook_server
 from tools import logger
 
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
 
 # Initialise Flask
 app = Flask(__name__)
 
 # Initialise the logger
 logger.init(app)
+logger.get_logger().info("Flask initialised")
 
-logger.get_logger().warning("Flask and logger initialised")
+# Add sentry integration
+logger.get_logger().info("Initialising Sentry")
+sentry_sdk.init(
+    dsn=SecretConstant.SENTRY_URL,
+    integrations=[FlaskIntegration()]
+)
 
 # Initialise Flask's configurations
-logger.get_logger().warning("Initialising the Flask's configurations")
+logger.get_logger().info("Initialising the Flask's configurations")
 config = {
     "development": "web_server.configuration.development_config.DevelopmentConfig",
     "prod": "web_server.configuration.prod_config.ProdConfig",
@@ -29,23 +37,26 @@ config_name = os.getenv('FLASK_CONFIGURATION', 'default')
 app.config.from_object(config[config_name])
 
 # Initialise Flask's json encoder
-logger.get_logger().warning("Initialising the Flask's json encoder")
+logger.get_logger().info("Initialising the Flask's json encoder")
 app.json_encoder = CustomJsonEncoder
 
 # Initialise the Flask's secret key
-logger.get_logger().warning("Initialising the Flask's secret key")
+logger.get_logger().info("Initialising the Flask's secret key")
 app.secret_key = SecretConstant.FLASK_APP_SECRET_KEY
 
 # Initialise Paranoid for an extra layer of security
-logger.get_logger().warning("Initialising Paranoid for an extra layer of security")
+logger.get_logger().info("Initialising Paranoid for an extra layer of security")
 paranoid = Paranoid(app)
 
 # Register the web server and the webhook to the Flask app
-logger.get_logger().warning("Registing the web server and the webhook to the Flask app")
+logger.get_logger().info("Registing the web server and the webhook to the Flask app")
+
 app.register_blueprint(webhook_server, url_prefix='/api')
 app.register_blueprint(web_server, url_prefix='/api')
 
 # Run the app
-logger.get_logger().warning("Running the app")
+logger.get_logger().info("Running the app")
+
+
 if __name__ == '__main__':
     app.run()
